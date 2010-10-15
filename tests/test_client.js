2741b44b3fce341b17515c131c9c792d64edc8f4
François de Metz <fdemetz@af83.com>
2010-10-15 03:46:23 +0200

    creating client with a path

--- tests/test_client.js
+++ tests/test_client.js
@@ -6,6 +6,11 @@ var assert = require('assert');
 // we test that
 var Client = require('spore').Client;
 
+// contruct with a filename
+var clientWithFile = new Client(__dirname +'/fixtures/test.json');
+assert.ok(clientWithFile.public_timeline, "clientWithFile should have a public_timeline method");
+
+// contruct with json
 var twitterClient = new Client({
         "api_base_url" : "http://api.twitter.com/1",
         "version" : "0.1",
@@ -24,12 +29,12 @@ var twitterClient = new Client({
         }
     });
 
-assert.ok(twitterClient.public_timeline, "twitterClient have a public_timeline method");
+assert.ok(twitterClient.public_timeline, "twitterClient should have a public_timeline method");
 // required param missing
 var called = 0;
 twitterClient.public_timeline({}, function(err, result) {
     called++;
-    assert.equal(null, result);
+    assert.equal(null, result, 'result should be null');
     assert.equal(err, 'format param is required');
 });
 assert.equal(called, 1, "callback should be called");
@@ -38,7 +43,7 @@ called = 0;
 twitterClient.public_timeline({format: 'json',
                                unknowparam: 'foo'}, function(err, result) {
     called++;
-    assert.equal(null, result);
+    assert.equal(null, result, 'result should be null');
     assert.equal(err, 'unknowparam param is unknow'); // very funny
 });
 assert.equal(called, 1, "callback should be called");
