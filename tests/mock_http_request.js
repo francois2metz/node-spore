1d6d758580b963456fb47d807cb89bfedfe7c2ba
François de Metz <francois@2metz.fr>
2011-02-04 17:31:13 +0100

    Update http request client for node 0.4.
    Currently https doesn't work. I'm working on it.

--- tests/mock_http_request.js
+++ tests/mock_http_request.js
@@ -12,62 +12,54 @@ var assert = require('assert');
 /**
  *
  */
-function createClient(request, port, host, secure, credentials) {
-    assert.equal(port, request.port);
-    assert.equal(host, request.host);
+function _request(request, options, callback) {
+    assert.equal(options.port, request.port);
+    assert.equal(options.host, request.host);
+    assert.equal(options.method, request.method);
+    assert.equal(options.path, request.path);
+    assert.equal(options.headers.host, request.host);
+    if (request.headers)
+        assert.deepEqual(options.headers, request.headers);
     if (request.secure !== undefined)
         assert.equal(secure, request.secure);
     return {
         _events: {},
+        data: "",
         on: function(name, callback) {
             this._events[name] = callback;
         },
-        request: function(method, path, headers) {
-            assert.equal(method, request.method);
-            assert.equal(path, request.path);
-            assert.equal(headers.host, request.host);
-            if (request.headers)
-                assert.deepEqual(headers, request.headers);
-            var client = this;
-            return {
-                _events: {},
-                on: function(name, callback) {
-                    this._events[name] = callback;
-                },
-                write: function(data) {
-                    this.data = data;
-                },
-                end: function() {
-                    var that = this;
-                    if (request.error) {
-                        setTimeout(function() {
-                            client._events['error'](request.error);
-                        }, 100);
-                        return;
+        write: function(data) {
+            this.data += data;
+        },
+        end: function() {
+            var that = this;
+            if (request.error) {
+                setTimeout(function() {
+                    that._events['error'](request.error);
+                }, 100);
+                return;
+            }
+            if (request.payload)
+                assert.equal(this.data, request.payload);
+            setTimeout(function() {
+                callback({
+                    statusCode: request.statusCode,
+                    headers: request.response_headers,
+                    _events: {},
+                    on: function(name, callback){
+                        this._events[name] = callback;
+                        if (name == 'end')
+                        {
+                            var that = this;
+                            setTimeout(function() {
+                                var data = request.response_data;
+                                that._events.data(data);
+                                that._events.end();
+                            }, 100);
+                        }
                     }
-                    if (request.payload)
-                        assert.equal(this.data, request.payload);
-                    setTimeout(function() {
-                        that._events.response({
-                            statusCode: request.statusCode,
-                            headers: request.response_headers,
-                            _events: {},
-                            on: function(name, callback){
-                                this._events[name] = callback;
-                                if (name == 'end')
-                                {
-                                    var that = this;
-                                    setTimeout(function() {
-                                        var data = request.response_data;
-                                        that._events.data(data);
-                                        that._events.end();
-                                    }, 100);
-                                }
-                            }
-                        });
-                    }, 1000);
-                }
-            };
+                });
+            }, 1000);
         }
     };
 }
@@ -76,9 +68,9 @@ exports.init = function() {
     var mocks = [];
     return {
         http : {
-            createClient: function(port, host, secure, credentials) {
+            request: function(options, callback) {
                 var request = mocks.shift();
-                return createClient(request, port, host, secure, credentials);
+                return _request(request, options, callback);
             }
         },
         add: function(mock) {
